a138c2141b0cfdcaba371b87e0a7984f7de221b9
Goldmember <francesco.paolo.galletta@gmail.com>
2022-02-22 19:11:02 +0000

    added etherscan validator and mainnet config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,17 +3,27 @@
  */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.9",
-  defaultNetwork: "ropsten",
+  defaultNetwork: "mainnet",
   networks: {
     hardhat: {},
     ropsten: {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "6V2TB93MF2FG7ER7CEEJGGJUCTQR4DEWHV"
+  }
 };
 
